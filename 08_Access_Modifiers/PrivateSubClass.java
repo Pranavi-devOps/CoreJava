421c7bd6bb55c08e44f9aa4cf5df3003449bdb8a
Pranavi-devOps <pranavi312006@gmail.com>
2026-02-20 10:53:30 +0530

    Updated logic in 02_Operators

--- 08_Access_Modifiers/PrivateSubClass.java
+++ 08_Access_Modifiers/PrivateSubClass.java
@@ -2,12 +2,6 @@ public class PrivateSubClass extends PrivateExample {
 
     public static void main(String[] args) {
 
-        PrivateSubClass obj = new PrivateSubClass();
-
-        // ❌ Cannot access private members directly
-        // System.out.println(obj.number); // ERROR
-        // obj.display(); // ERROR
-
-        System.out.println("Cannot access private members in subclass");
+        System.out.println("Cannot access private members");
     }
 }
--- 08_Access_Modifiers/ProtectedDifferentPackageTest.java
+++ 08_Access_Modifiers/ProtectedDifferentPackageTest.java
@@ -2,12 +2,6 @@ public class ProtectedDifferentPackageTest {
 
     public static void main(String[] args) {
 
-        ProtectedExample obj = new ProtectedExample();
-
-        // ❌ Cannot access directly
-        // System.out.println(obj.number);
-        // obj.display();
-
         System.out.println("Cannot access protected members in different package without inheritance");
     }
 }
